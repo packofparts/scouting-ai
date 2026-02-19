141c0ec341982865b1ce073118ff6bdf94614b2b
Changhyung Lee <makerchlee@outlook.kr>
2026-02-18 19:52:48 -0800

    Fixes

--- run.sh
+++ run.sh
@@ -1,24 +1,37 @@
 #!/bin/bash
 
-#Shoutout to Gemini for converting this from run.bat to run.sh!!!
 cd "$(dirname "$0")"
 
-set -o pipefail
+echo "Welcome to The P.A.C.K. (Predictive, Analytical, and Competitive Knowledge-base)!"
 
-read -p "Need 2 run Video Downloader? (y/n): " download
+read -p "Need 2 run Video Downloader? (y/n) " download
 if [[ "$download" =~ ^[Yy]$ ]]; then
-    python3.12 YouTubeDownloader.py
-    
+
+    read -p "Enter Video URL: " URL
+
+    rm -f "matches/match.mp4"
+    yt-dlp -o "matches/match.mp4" -f "bestvideo[height=720][ext=mp4]" --cookies "cookies" "$URL"
+
+    if [ $? -ne 0 ]; then
+        echo "Video Downloader failed with error code $?"
+        exit $?
+    fi
 fi
 
-read -p "Need 2 run Detector? (y/n): " detect
+read -p "Need 2 run Detector? (y/n) " detect
 if [[ "$detect" =~ ^[Yy]$ ]]; then
-    python3.12 detect2.py
-    
+
+    python3 detect2.py
+
+    if [ $? -ne 0 ]; then
+        echo "Detector failed with error code $?"
+        exit $?
+    fi
 fi
 
-read -p "Need 2 run Analyzer? (y/n): " analyze
+read -p "Need 2 run Analyzer? (y/n) " analyze
 if [[ "$analyze" =~ ^[Yy]$ ]]; then
+
     echo "Enter team numbers of the FRC robots that you are scouting"
     echo "1. in order from closest to furthest away from camera"
     echo "2. separated by spaces"
@@ -28,7 +41,10 @@ if [[ "$analyze" =~ ^[Yy]$ ]]; then
     read -p "Blue Alliance: " blue
 
     javac src/*.java json/*.java
-    
     java -cp "src:json" AIScout $red $blue
 
+    if [ $? -ne 0 ]; then
+        echo "Analyzer failed with error code $?"
+        exit $?
+    fi
 fi
